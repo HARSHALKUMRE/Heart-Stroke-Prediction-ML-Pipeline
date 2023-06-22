e281b5b01b4a87c1b908ca575818557ab7fe58d6
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-06-22 18:20:35 +0530

    project setup file & requirements file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Heart-Stroke-Predictior",
+    version="0.1.1",
+    author="Harshal Kumre",
+    author_email="kumreharshalkumar@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
